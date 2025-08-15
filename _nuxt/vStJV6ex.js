a816ef7b54c0533e14b17432857df598c5ae7c9a
Arvin-cmd <56837443+Arvin-cmd@users.noreply.github.com>
2025-08-15 23:36:36 +0800

    123

---
+++ _nuxt/vStJV6ex.js
@@ -0,0 +1,16 @@
+import{L as f,M as a,N as m}from"./DMKSc2c0.js";import{c as p}from"./C6AA0AkK.js";import"./Byt6dQFO.js";import"./u2D-QnjG.js";import"./l-UjSJ9P.js";import"./DWzFNXDx.js";import"./DjwJKeUN.js";import"./DjOCkDgc.js";const d=f`
+  :host > wui-flex:first-child {
+    height: 500px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: none;
+  }
+
+  :host > wui-flex:first-child::-webkit-scrollbar {
+    display: none;
+  }
+`;var u=function(o,t,i,r){var n=arguments.length,e=n<3?t:r===null?r=Object.getOwnPropertyDescriptor(t,i):r,l;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")e=Reflect.decorate(o,t,i,r);else for(var s=o.length-1;s>=0;s--)(l=o[s])&&(e=(n<3?l(e):n>3?l(t,i,e):l(t,i))||e);return n>3&&e&&Object.defineProperty(t,i,e),e};let c=class extends a{render(){return m`
+      <wui-flex flexDirection="column" .padding=${["0","m","m","m"]} gap="s">
+        <w3m-activity-list page="activity"></w3m-activity-list>
+      </wui-flex>
+    `}};c.styles=d;c=u([p("w3m-transactions-view")],c);export{c as W3mTransactionsView};
