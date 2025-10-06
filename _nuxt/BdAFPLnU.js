63e4203cb3317c0f3c1b507130742465ca14c964
Arvin-cmd <56837443+Arvin-cmd@users.noreply.github.com>
2025-10-06 16:04:14 +0800

    123

--- _nuxt/BdAFPLnU.js
+++
@@ -1,5 +0,0 @@
-import{cV as r}from"./BWftuhBA.js";/**
- * @license
- * Copyright 2018 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */const c=o=>o??r;export{c as o};
---
+++ _nuxt/KynGGuU6.js
@@ -0,0 +1,16 @@
+import{bz as f,bA as a,bD as m}from"./du6FXzWk.js";import{c as p}from"./Cj3DTtSp.js";import"./D_GqHUlY.js";import"./D74UfSbd.js";import"./kMR66_7O.js";import"./D-jXdOs4.js";import"./DtK7yry9.js";import"./D2hzk5PQ.js";const d=f`
+  :host > wui-flex:first-child {
+    height: 500px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: none;
+  }
+
+  :host > wui-flex:first-child::-webkit-scrollbar {
+    display: none;
+  }
+`;var u=function(o,t,i,r){var n=arguments.length,e=n<3?t:r===null?r=Object.getOwnPropertyDescriptor(t,i):r,l;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")e=Reflect.decorate(o,t,i,r);else for(var s=o.length-1;s>=0;s--)(l=o[s])&&(e=(n<3?l(e):n>3?l(t,i,e):l(t,i))||e);return n>3&&e&&Object.defineProperty(t,i,e),e};let c=class extends a{render(){return m`
+      <wui-flex flexDirection="column" .padding=${["0","m","m","m"]} gap="s">
+        <w3m-activity-list page="activity"></w3m-activity-list>
+      </wui-flex>
+    `}};c.styles=d;c=u([p("w3m-transactions-view")],c);export{c as W3mTransactionsView};
